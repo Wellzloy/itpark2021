70f3b676725b371ba5618ef835a20d55254bda1b
Wellzloy <well1970@mail.ru>
2021-11-07 22:27:01 +0300

    add wis lessons

--- src/main/java/lesson1/FirstProgram.java
+++ src/main/java/lesson1/FirstProgram.java
@@ -1,5 +1,6 @@
-public class FirstProgram {
+package lesson1;
 
+public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Привет мир третий раз");
     }
---
+++ src/main/java/lesson2/FerstProgram.java
@@ -0,0 +1,48 @@
+package lesson2;
+
+public class FerstProgram {
+
+    int arg1, arg2;
+
+    public static void main(String[] args) {
+
+        long summa = summa(3, 5);
+        System.out.println(summa);
+
+        long otherSumma = summa(5, 5);
+        System.out.println(otherSumma);
+
+        long yetAnotherSumma = summa(2, 18);
+        System.out.println(yetAnotherSumma);
+
+        long resultOfMinus = minus(2, 18);
+        System.out.println(resultOfMinus);
+
+        long result = multiplyAndSumma(5);
+        System.out.println(result);
+
+    }
+
+    /**
+     * @param arg1 первый аргумент
+     * @param arg2 второй аргумент
+     * @return результат сложения двух чисел
+     */
+    public static long summa(int arg1, int arg2) {
+        int result = arg1 + arg2;
+        return result;
+    }
+
+    public static long minus(int arg1, int arg2) {
+        return arg1 - arg2;
+    }
+
+    public static long multiplyAndSumma(int arg1) {
+        int result = 3 * arg1 + 15;
+        result = result - 38;
+        return result;
+    }
+
+}
+
+
