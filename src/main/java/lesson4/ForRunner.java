60af68af9d8efc36af9d8b5806202319e2d1990f
Wellzloy <well1970@mail.ru>
2021-11-17 00:55:09 +0300

    add lesson4 oll

---
+++ src/main/java/lesson4/ForRunner.java
@@ -0,0 +1,10 @@
+package lesson4;
+
+public class ForRunner {
+    public static void main(String[] args) {
+        for (int counter =1;counter <= 100; counter++){
+            System.out.println ("Текущее значение счетчика:" + counter);
+        }
+        System.out.println ("---------------");
+    }
+}
